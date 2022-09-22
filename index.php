1849da53d3d4f012e12e3f980ef9dd002762931f
dondiego <jokonito06@gmail.com>
2022-09-22 12:08:59 +0200

    Addind 5rem marge

--- index.php
+++ index.php
@@ -16,14 +16,24 @@
     <header class="container">
     <h1>Mini jeu de combats</h1>
         <nav class="nav">
-        <?php
-        require ('menu.php');
-        ?>
-         </nav>
+            <?php
+            require ('menu.php');
+            ?>
+        </nav>
     </header>
 
     <main class="container">
+        <div class="row">
+            <div class="col-12 mt-5">
+                <?php
+                $sql = "SELECT COUNT(*) FROM personnages"; 
+                $response = $db->query( $sql );
+                $nbPerso = $response->fetch();
 
+                echo ("<p> Nombre de personnages crées : " . $nbPerso[0] . "</p>");
+                ?>
+            </div>
+        </div>
     </main>
 
     <footer class="container">
